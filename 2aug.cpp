5b288bb5fff6a7e955d29429c4604b5459370755
Vipul Singh <120162007+VIPULSINGH18@users.noreply.github.com>
2025-08-18 12:47:04 +0530

    Add files via upload

---
+++ 2aug.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL || q==NULL){
+            if(p==q){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        bool same_left= isSameTree(p->left,q->left);
+        bool same_right= isSameTree(p->right,q->right);
+        return same_left && same_right && p->val==q->val;
+    }
+};
