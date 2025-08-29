a73a89149a2d315f8bd1da5d2434a531cff849ac
Vipul Singh <120162007+VIPULSINGH18@users.noreply.github.com>
2025-08-29 23:53:02 +0530

    Add files via upload

---
+++ 10aug.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<TreeNode*,unsigned long long>>q;
+        q.push({root,0});
+        int max_width=0;
+
+        while(q.size()>0){
+            int curr_level= q.size();
+            unsigned long long st_idx= q.front().second;
+            unsigned long long e_idx= q.back().second;
+
+            max_width= max(max_width,(int)(e_idx-st_idx+1));
+            for(int i=0;i<curr_level;i++){
+            
+        
+            TreeNode* curr=q.front().first;
+            unsigned long long idx= q.front().second;
+            q.pop();
+            
+                if(curr->left!=NULL){
+                    q.push({curr->left,idx*2+1});
+                }
+                if(curr->right!=NULL){
+                    q.push({curr->right,idx*2+2});
+                }
+            }
+        }
+        return max_width;
+        
+    }
+};
